d5937330a6b9f9169336b90de40f5d2c2e69f122
reza gholamalitabar <reza.gh.fi@gmail.com>
2022-02-10 14:45:03 +0330

    change

---
+++ src/jalase2_classDefine/Vazir.java
@@ -0,0 +1,12 @@
+package jalase2_classDefine;
+
+public class Vazir {
+    public void antekhabeMoaven() {
+        System.out.println("ana antekhabeMoaven");
+    }
+
+    public void antekhabeMonshi() {
+        System.out.println("ana antekhabeMonshi");
+    }
+
+}
