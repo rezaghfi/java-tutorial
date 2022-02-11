c564b9776e93a8e5f58bd233fc2863d153263093
reza gholamalitabar <reza.gh.fi@gmail.com>
2022-02-11 11:43:26 +0330

    change

--- src/jalase2_classDefine/Main.java
+++ src/jalase2_classDefine/Main.java
@@ -1,4 +1,17 @@
 package jalase2_classDefine;
 
 public class Main {
+    public static void main(String[] args) {
+        int x=12,y=12,z=12,f=12;
+        Vazir naft = new Vazir();
+        Vazir omor = new Vazir();
+        naft.antekhabeMoaven();
+        omor.antekhabeMonshi();
+        naft.sum(12,23);
+        naft.sum(12,12,12);
+        naft.sum(12.3,12.3);
+        int result = naft.sum(x,y,z,f);
+        System.out.println("summation of "+x+" + "+y+" + "+z+" + "+f+" = "+result);
+
+    }
 }
--- src/jalase2_classDefine/Vazir.java
+++ src/jalase2_classDefine/Vazir.java
@@ -1,12 +1,29 @@
 package jalase2_classDefine;
 
 public class Vazir {
+    public String fistName;
+    public String lastName;
+    public int age;
     public void antekhabeMoaven() {
         System.out.println("ana antekhabeMoaven");
     }
 
     public void antekhabeMonshi() {
         System.out.println("ana antekhabeMonshi");
+    }
+
+    // overloaded Methods
+    public void sum(int x, int y){
+        System.out.println("summation of "+x+" + "+y+" = "+(x+y));
+    }
+    public void sum(double x, double y){
+        System.out.println("summation of "+x+" + "+y+" = "+(x+y));
+    }
+    public void sum(int x, int y, int z){
+        System.out.println("summation of "+x+" + "+y+" + "+z+" = "+(x+y+z));
+    }
+    public int sum(int x, int y, int z, int f){
+        return x+y+z+f;
     }
 
 }
